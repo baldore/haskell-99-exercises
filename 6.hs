d6b81b8185fd28d4095bb8057b9de19dc27be8ba
Camilo Orrego <acornssn@gmail.com>
2015-09-24 23:31:24 -0500

    Add 6.hs

---
+++ 6.hs
@@ -0,0 +1,14 @@
+import Test.Hspec
+
+-- Problem 6
+-- Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
+
+main :: IO()
+main = hspec $
+  describe "99-exercises.6 = Is a palindrome" $
+    it "should check if a list is a palyndrome" $ do
+      isPalindrome [1,2,3] `shouldBe` False
+      isPalindrome "madamimadam" `shouldBe` True
+      isPalindrome [1,2,4,8,16,8,4,2,1] `shouldBe` True
