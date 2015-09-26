86d5cc97513c764bae18981b6a64b021e94d04c6
Camilo Orrego <acornssn@gmail.com>
2015-09-26 18:13:12 -0500

    Improve 7.hs

--- 7.hs
+++ 7.hs
@@ -1,4 +1,4 @@
-import Test.Hspec
+import           Test.Hspec
 
 -- Problem 7
 -- Flatten a nested list structure.
@@ -7,14 +7,16 @@ import Test.Hspec
 data NestedList a = Elem a | List [NestedList a]
 
 flatten :: NestedList a -> [a]
-flatten (Elem x) = [x]
-flatten (List []) = []
-flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten ls = flatten' ls []
+  where
+    flatten' (Elem x)      acc = x:acc
+    flatten' (List [])     acc = acc
+    flatten' (List (x:xs)) acc = flatten' x $ flatten' (List xs) acc
 
 main :: IO()
 main = hspec $
   describe "99-exercises.7 = Flatten a nested list structure" $
     it "should return the number of elemnents of a list" $ do
       flatten (Elem 5) `shouldBe` [5]
-      flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) `shouldBe` [1,2,3,4,5]
       flatten (List ([] :: [NestedList Int])) `shouldBe` []
+      flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5, List[]]]) `shouldBe` [1,2,3,4,5]
