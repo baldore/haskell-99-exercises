42254ca7f2837241009f15993e5f350496ce4410
Camilo Orrego <acornssn@gmail.com>
2015-09-26 23:57:15 -0500

    Add 9.hs

---
+++ 9.hs
@@ -0,0 +1,15 @@
+
+import           Test.Hspec
+
+-- Problem 9
+-- Pack consecutive duplicates of list elements into sublists.
+-- If a list contains repeated elements they should be placed in separate sublists.
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack ls@(x:_) = takeWhile (== x) ls : pack(dropWhile (==x) ls)
+
+main :: IO()
+main = hspec $
+  describe "99-exercises.9 = Pack consecutive similar elements" $
+    it "should remove the consecutive repeated elements" $
+      pack "aaaabccaadeeee" `shouldBe` ["aaaa","b","cc","aa","d","eeee"]
