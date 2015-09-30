254f337793f7f06fbbc4914ca4e2b3a0c9725f04
Camilo Orrego <aorrego@hugeinc.com>
2015-09-30 14:23:55 -0500

    Improve 16.hs answer

--- 16.hs
+++ 16.hs
@@ -3,12 +3,11 @@ import Test.Hspec
 -- Problem 16
 -- Drop every N'th element from a list.
 dropEvery :: Eq a => [a] -> Int -> [a]
-dropEvery ls n = reverse $ get' ls [] n n
+dropEvery ls n = get' ls n n
   where
-    get' [] acc _ _ = acc
-    get' (x:xs) acc t i
-      | i == 1    = get' xs acc t t
-      | otherwise = get' xs (x:acc) t (i-1)
+    get' [] _ _     = []
+    get' (_:xs) t 1 = get' xs t t
+    get' (x:xs) t i = x : get' xs t (i-1)
 
 main :: IO()
 main = hspec $
