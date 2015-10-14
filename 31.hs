7e3410920ef8973d4ecc385dfaf047e6c3ebc5a4
Camilo Orrego <aorrego@hugeinc.com>
2015-10-14 13:44:50 -0500

    Add 31.hs

---
+++ 31.hs
@@ -0,0 +1,15 @@
+import Test.Hspec
+
+-- Problem 31
+-- Determine whether a given integer number is prime.
+isPrime :: Int -> Bool
+isPrime n = primes == 1
+  where primes = length $ filter ((==) 0 . mod n) [2..n]
+
+main :: IO()
+main = hspec $
+  describe "99-exercises.31 = is prime number?" $
+    it "should determine if a number is prime" $ do
+      isPrime 7  `shouldBe` True
+      isPrime 10 `shouldBe` False
+      isPrime 13 `shouldBe` True
